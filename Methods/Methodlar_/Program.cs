8b8bc61ee228bb144404eabc912aa7abeec50bab
AlparslanKutay <98888034+AlparslanKutay@users.noreply.github.com>
2022-02-08 04:15:47 +0300

    Add files via upload

---
+++ Methods/Methodlar_/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodlar_
+{
+    class Program
+    {
+        public static void HesapMakinesi()
+        {
+
+            Console.WriteLine("Welcome Sir!!!");
+            string symbol;
+
+            double sayı;
+            double sayı2;
+
+           
+
+            Console.WriteLine("Please enter the symbol for which you would like the expression");
+
+            symbol = Console.ReadLine();
+
+            if (symbol == "+")
+            {
+                Console.WriteLine("Type the numbers");
+
+                sayı = int.Parse(Console.ReadLine());
+                sayı2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(sayı + sayı2 );
+            }
+
+            else if (symbol == "-")
+            {
+                Console.WriteLine("Type the numbers");
+
+                sayı = int.Parse(Console.ReadLine());
+                sayı2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(sayı - sayı2 );
+            }
+
+            else if (symbol == "*")
+            {
+                Console.WriteLine("Type the numbers");
+
+                sayı = int.Parse(Console.ReadLine());
+                sayı2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(sayı * sayı2 );
+            }
+
+            else if (symbol == "/")
+            {
+                Console.WriteLine("Type the numbers");
+
+                sayı = int.Parse(Console.ReadLine());
+                sayı2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(sayı / sayı2);
+            }
+
+            else
+            {
+                Console.WriteLine("Wrong input");
+            }
+
+        }
+        public static void HelloWorld()
+        {
+            Console.WriteLine("Yarabbi Şükür");
+        }
+
+        public static void SayıHesaplama()
+        {
+            int[] sayılar = new int[5];
+
+
+            for (int i = 0; i < sayılar.Length; i++)
+            {
+                Console.WriteLine("Lütfen bir sayı giriniz:");
+
+                sayılar[i] = int.Parse(Console.ReadLine());
+
+
+            }
+
+            foreach (var item in sayılar)
+            {
+                Console.WriteLine(item);
+            }
+
+            
+
+        }
+
+        public static void AlanHesaplama()
+        {
+            int a;
+            int b;
+            int alan;
+
+            Console.WriteLine("Lütfen kısa kenarı girin:");
+
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Lütfen uzun kenarı girin:");
+
+            b = Convert.ToInt32(Console.ReadLine());
+
+            alan = a * b;
+
+            Console.WriteLine(alan);
+        }
+
+
+        static void Main(string[] args)
+        {
+            //AlanHesaplama();
+
+            // SayıHesaplama();
+
+            HesapMakinesi();
+
+            Console.Read();
+        }
+    }
+}
---
+++ Methods/Methodlar_devam/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodlar_devam
+{
+    class Program
+    {
+        // bir şirketin default olarak 5 tane ürünü olsun, bu ürünlerin kontrolünü sağlayabilecekleri bir console uygulaması yazmalısın
+        // ürünlerin silinebilmesi, kaç ürün olduğunu görebilmeliler, ürün arayabilme, ürün ekleme
+
+        //ürün ekle , sil, sayı göster, ürünleri ismiyle arayabilme 
+
+
+        public static void dManage()
+        {
+
+
+
+            
+            
+
+            //Bu bir gıda şirketidir
+            string pro_1 = "Rice";
+            string pro_2 = "Bean";
+            string pro_3 = "Corn";
+            string pro_4 = "Eggplant";
+            string pro_5 = "Chickpea";
+
+            List<String> products = new List<string>();
+
+            products.Add(pro_1);
+            products.Add(pro_2);
+            products.Add(pro_3);
+            products.Add(pro_4);
+            products.Add(pro_5);
+
+
+
+
+            Console.WriteLine("Welcome Sir-------What you wanna do today?");
+
+            foreach (var item in products)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("---------------------");
+
+            Console.WriteLine("Wanna Add new product? Press A");
+
+            Console.WriteLine("Wanna Remove product? Press D");
+
+            Console.WriteLine("Wanna Show the Products? Press S" );
+
+            Console.WriteLine("Wanna  search any product? Press F");
+
+
+
+            string symbol;
+
+            symbol = Console.ReadLine();
+
+            
+
+            
+
+
+            if (symbol == "A")
+            {
+                Console.WriteLine("What you wanna to add our product list");
+
+                products.Add(Console.ReadLine());
+
+                foreach (var item in products)
+                {
+                    Console.WriteLine(item);
+                }
+
+            }
+
+            else if (symbol == "D")
+            {
+                string delete;
+                products.Count();
+
+                
+
+                Console.WriteLine("Which product wanna remove from our list?");
+
+                delete = Console.ReadLine();
+
+                products.Remove(delete);
+
+                foreach (var item in products)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            else if (symbol == "S")
+            {
+                Console.WriteLine("Here to our list");
+
+                products.Count();
+
+                foreach (var item in products)
+                {
+                    Console.WriteLine(item);
+                }
+
+            }
+
+            else if (symbol == "F")
+            {
+                Console.WriteLine("Which product wanna find");
+
+                string arama;
+                arama = Console.ReadLine();
+
+                products.Contains(arama);
+
+                foreach (var item in products)
+                {
+                    Console.WriteLine(item);
+                }
+
+            }
+
+            foreach (var item in products)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            dManage();
+
+            Console.ReadKey();
+        }
+    }
+}
